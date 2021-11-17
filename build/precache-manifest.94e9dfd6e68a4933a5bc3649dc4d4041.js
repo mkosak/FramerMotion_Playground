fc7e939706ca35bfbe16b4394bfa51b867ec5d65
Maksym Kosak <maksimkosak@gmail.com>
2021-11-17 20:37:46 +0200

    Update folders structure

---
+++ build/precache-manifest.94e9dfd6e68a4933a5bc3649dc4d4041.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac5cd144d4bd7ce7d017",
+    "url": "/FramerMotion_Playground/static/css/main.35bfe33f.chunk.css"
+  },
+  {
+    "revision": "ac5cd144d4bd7ce7d017",
+    "url": "/FramerMotion_Playground/static/js/main.ac5cd144.chunk.js"
+  },
+  {
+    "revision": "8ae3b60c1d16697f7d1d",
+    "url": "/FramerMotion_Playground/static/js/1.8ae3b60c.chunk.js"
+  },
+  {
+    "revision": "e9f40e50157f6e6cc452",
+    "url": "/FramerMotion_Playground/static/js/runtime~main.e9f40e50.js"
+  },
+  {
+    "revision": "2d6e510df77c7ff3c252b7a82ba93d66",
+    "url": "/FramerMotion_Playground/static/media/Table_Long.2d6e510d.svg"
+  },
+  {
+    "revision": "47ffa3612680b9e3807c987f9ffc86e3",
+    "url": "/FramerMotion_Playground/static/media/Table_13_Round.47ffa361.svg"
+  },
+  {
+    "revision": "6e443e0561f690b3d0afcd9fe539d1f0",
+    "url": "/FramerMotion_Playground/static/media/Table_2.6e443e05.svg"
+  },
+  {
+    "revision": "72493d0182df9101114befa38f2506ff",
+    "url": "/FramerMotion_Playground/static/media/Plant.72493d01.svg"
+  },
+  {
+    "revision": "118f979d470d7c8d83d298b9c426f982",
+    "url": "/FramerMotion_Playground/index.html"
+  }
+];
